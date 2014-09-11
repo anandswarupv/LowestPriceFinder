09ad215c7b453ecfb9a017b6b57ebad4110f3aaf
Pratik <pgarg@xebia.com>
2014-09-11 16:10:55 +0530

    Added another scenario for legacy

--- src/main/java/com/xebia/tdd/training/hotelreservation/legacy2/Email.java
+++ src/main/java/com/xebia/tdd/training/hotelreservation/legacy2/Email.java
@@ -2,8 +2,25 @@ package com.xebia.tdd.training.hotelreservation.legacy2;
 
 public class Email {
 
-	public Email(String string) {
-		// TODO Auto-generated constructor stub
+	private String subject;
+	private Integer nextAvailableNumber;
+	private String message;
+
+	public Email(String message) {
+		this.message = message;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setUniqueId(Integer nextAvailableNumber) {
+		this.nextAvailableNumber = nextAvailableNumber;
+		
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
 	}
 
 }
---
+++ src/main/java/com/xebia/tdd/training/hotelreservation/legacy3/DBUtils.java
@@ -0,0 +1,9 @@
+package com.xebia.tdd.training.hotelreservation.legacy3;
+
+
+public class DBUtils {
+
+	public static Integer getNextAvailableNumber(){
+		throw new RuntimeException("I Will contact database");
+	}
+}
