7eb79bed14b768a1b5fd60cbf43126c9dd7e5217
Pratik <pgarg@xebia.com>
2014-09-24 12:18:14 +0530

    A

--- src/main/java/com/xebia/tdd/training/hotelreservation/chapter4/HotelEmployee.java
+++ src/main/java/com/xebia/tdd/training/hotelreservation/chapter4/HotelEmployee.java
@@ -6,7 +6,10 @@ public class HotelEmployee {
 
 	private Integer serialid = 0;
 	private Date modifiedDate;
-
+	private String employeeName;
+	private Integer employeeAge;
+	private String employeeCountry;
+	
 	public void setSerialId(Integer serialid) {
 		this.serialid = serialid;
 	}
@@ -26,6 +29,30 @@ public class HotelEmployee {
 
 	public Date getModifiedDate() {
 		return modifiedDate;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public Integer getEmployeeAge() {
+		return employeeAge;
+	}
+
+	public void setEmployeeAge(Integer employeeAge) {
+		this.employeeAge = employeeAge;
+	}
+
+	public String getEmployeeCountry() {
+		return employeeCountry;
+	}
+
+	public void setEmployeeCountry(String employeeCountry) {
+		this.employeeCountry = employeeCountry;
 	}
 
 }
